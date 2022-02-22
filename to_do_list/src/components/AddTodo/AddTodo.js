f11414e193ac90b2842d25beca0f96744dbc5305
FlyStr1ker <Ketraofsaman@gmail.com>
2022-02-22 17:10:45 +0200

    Version 2.0

---
+++ to_do_list/src/components/AddTodo/AddTodo.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react"
+import { nanoid } from 'nanoid'
+import {Row , Col , Button, FormControl} from 'react-bootstrap'
+import s from './AddTodo.module.css'
+
+
+function AddTodo ({todo , setTodo}){
+
+     const [value, setValue] = useState ('')
+
+
+     function saveTodo(){
+         setTodo(
+      [...todo, {
+          id: nanoid(),
+          title: value,
+          status : true
+      }]
+  )
+  setValue('')
+}
+
+
+    return (
+       <Row>
+           <Col className={s.addTodoForm}>
+           <FormControl placeholder="введите задачу" value={value} onChange={ (e)=>setValue(e.target.value)} />
+            <Button variant="primary" className={s.btn}  onClick={saveTodo}>Сохранить</Button>
+           </Col>
+       </Row>
+    )
+}
+
+export default AddTodo
